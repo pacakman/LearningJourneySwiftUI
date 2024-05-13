7b7fb7001ea04fba0a5e7416497f9a126810f46f
Idris <idrisnovandi@klikdokter.com>
2024-05-13 10:35:38 +0700

    init bagian 2

---
+++ Bagian 2/HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Idris on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
