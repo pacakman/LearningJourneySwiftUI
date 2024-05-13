66ece2db9c9d49772a7d40b9c166ecfc6e6c3c4b
Idris <idrisnovandi@klikdokter.com>
2024-05-13 15:03:36 +0700

    finish bagian 2

--- Bagian 2/HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ Bagian 2/HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Hello, world!")
+                    .multilineTextAlignment(.leading)
+                HStack {
+                    Text("Hello, world!")
+                    Spacer()
+                    Text("Hello, world!")
+                }
+                .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                .foregroundColor(.yellow)
+
+                Image(systemName: "person")
+                    .imageScale(.large)
+                    .foregroundColor(.black)
+                    .background(.blue)
+                Spacer()
+                Divider()
+            }
+            .padding()
+            .navigationTitle(Text("Beranda"))
         }
-        .padding()
+
     }
 }
 
